ecfa3edcd60a4dbe163874fd2b70cde2ab9a39c5
wojciech-sak <wojciech.r.sak@gmail.com>
2021-11-08 10:52:29 +0100

    add server port, add log info for controller

--- src/main/java/com/kodilla/accounts/controller/AccountsController.java
+++ src/main/java/com/kodilla/accounts/controller/AccountsController.java
@@ -8,7 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 @Slf4j
@@ -31,6 +34,7 @@ public class AccountsController {
 
     @GetMapping
     public GetAccountsResponse getCustomerAccounts(@RequestParam Long customerId) {
+        log.info("Get accounts for customerId: {}", customerId);
         if(!allowGetAccounts) {
             log.info("Getting accounts is disabled");
             throw new ResponseStatusException(HttpStatus.SERVICE_UNAVAILABLE, "Getting accounts is disabled");
